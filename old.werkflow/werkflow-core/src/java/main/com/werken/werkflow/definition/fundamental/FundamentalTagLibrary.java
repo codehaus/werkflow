d83b86507a51160429b015947c76274d82610b23
bob <bob@b84b0c9b-c22a-0410-9a26-ab48ae73d5df>
2003-01-21 08:14:59 +0000

    Tweaking correlation to work.
    Re-arranged the logic within MessageWaiterCorrelator to avoid
    recursion and ConcurrentModificationExceptions.

--- old.werkflow/werkflow-core/src/java/main/com/werken/werkflow/definition/fundamental/FundamentalTagLibrary.java
+++ old.werkflow/werkflow-core/src/java/main/com/werken/werkflow/definition/fundamental/FundamentalTagLibrary.java
@@ -33,5 +33,8 @@ public class FundamentalTagLibrary
 
         registerTag( "message-type",
                      MessageTypeTag.class );
+
+        registerTag( "message",
+                     MessageTag.class );
     }
 }
