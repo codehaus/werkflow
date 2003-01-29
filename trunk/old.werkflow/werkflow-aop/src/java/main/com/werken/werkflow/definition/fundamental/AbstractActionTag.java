67ae878aa81e72955bbee2e8a2e863fe256d06a2
bob <bob@b84b0c9b-c22a-0410-9a26-ab48ae73d5df>
2003-01-29 22:52:46 +0000

    Further cleaning and removal of half-baked thoughts.

--- trunk/old.werkflow/werkflow-aop/src/java/main/com/werken/werkflow/definition/fundamental/AbstractActionTag.java
+++ trunk/old.werkflow/werkflow-aop/src/java/main/com/werken/werkflow/definition/fundamental/AbstractActionTag.java
@@ -1,31 +1,104 @@
 package com.werken.werkflow.definition.fundamental;
 
+/*
+ $Id$
+
+ Copyright 2003 (C) The Werken Company. All Rights Reserved.
+ 
+ Redistribution and use of this software and associated documentation
+ ("Software"), with or without modification, are permitted provided
+ that the following conditions are met:
+
+ 1. Redistributions of source code must retain copyright
+    statements and notices.  Redistributions must also contain a
+    copy of this document.
+ 
+ 2. Redistributions in binary form must reproduce the
+    above copyright notice, this list of conditions and the
+    following disclaimer in the documentation and/or other
+    materials provided with the distribution.
+ 
+ 3. The name "werkflow" must not be used to endorse or promote
+    products derived from this Software without prior written
+    permission of The Werken Company.  For written permission,
+    please contact bob@werken.com.
+ 
+ 4. Products derived from this Software may not be called "werkflow"
+    nor may "werkflow" appear in their names without prior written
+    permission of The Werken Company. "werkflow" is a registered
+    trademark of The Werken Company.
+ 
+ 5. Due credit should be given to The Werken Company.
+    (http://werkflow.werken.com/).
+ 
+ THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS
+ ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
+ NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ THE WERKEN COMPANY OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ */
+
 import com.werken.werkflow.action.Action;
 
 import org.apache.commons.jelly.TagSupport;
 import org.apache.commons.jelly.JellyException;
 
+/** Base for custom <code>Action</code> tags.
+ *
+ *  <p>
+ *  Due to the pluggable nature of werkflow semantics, the
+ *  <code>AbstractActionTag</code> assists in weaving custom
+ *  <code>Action</code>s into the fundamental syntax.
+ *  </p>
+ *
+ *  <p>
+ *  The custom tag should perform whatever is required to
+ *  construct an <code>Action</code> in the {@link org.apache.commons.jelly.Tag#doTag}
+ *  method, and finally call {@link #setAction} on this class
+ *  to install the action into the appropriate library or task.
+ *  </p>
+ *
+ *  @see Action
+ *
+ *  @author <a href="mailto:bob@eng.werken.com">bob mcwhirter</a>
+ *
+ *  @version $Id$
+ */
 public abstract class AbstractActionTag
     extends TagSupport
 {
-    private String id;
+    // ----------------------------------------------------------------------
+    //     Constructors
+    // ----------------------------------------------------------------------
 
+    /** Construct.
+     */
     public AbstractActionTag()
     {
+        // intentionally left blank.
     }
 
-    public void setId(String id)
-    {
-        this.id = id;
-    }
+    // ----------------------------------------------------------------------
+    //     Instance methods
+    // ----------------------------------------------------------------------
 
-    public String getId()
-    {
-        return this.id;
-    }
-
+    /** Install an <code>Action</code>.
+     *
+     *  @param action The action.
+     *
+     *  @throws JellyException If the tag is not used within the correct
+     *          context.
+     */
     public void setAction(Action action)
-        throws Exception
+        throws JellyException
     {
         ActionReceptor receptor = (ActionReceptor) findAncestorWithClass( ActionReceptor.class );
 
--- trunk/old.werkflow/werkflow-aop/src/java/main/com/werken/werkflow/definition/fundamental/AbstractMessageCorrelatorTag.java
+++ trunk/old.werkflow/werkflow-aop/src/java/main/com/werken/werkflow/definition/fundamental/AbstractMessageCorrelatorTag.java
@@ -1,20 +1,104 @@
 package com.werken.werkflow.definition.fundamental;
 
+/*
+ $Id$
+
+ Copyright 2003 (C) The Werken Company. All Rights Reserved.
+ 
+ Redistribution and use of this software and associated documentation
+ ("Software"), with or without modification, are permitted provided
+ that the following conditions are met:
+
+ 1. Redistributions of source code must retain copyright
+    statements and notices.  Redistributions must also contain a
+    copy of this document.
+ 
+ 2. Redistributions in binary form must reproduce the
+    above copyright notice, this list of conditions and the
+    following disclaimer in the documentation and/or other
+    materials provided with the distribution.
+ 
+ 3. The name "werkflow" must not be used to endorse or promote
+    products derived from this Software without prior written
+    permission of The Werken Company.  For written permission,
+    please contact bob@werken.com.
+ 
+ 4. Products derived from this Software may not be called "werkflow"
+    nor may "werkflow" appear in their names without prior written
+    permission of The Werken Company. "werkflow" is a registered
+    trademark of The Werken Company.
+ 
+ 5. Due credit should be given to The Werken Company.
+    (http://werkflow.werken.com/).
+ 
+ THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS
+ ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
+ NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ THE WERKEN COMPANY OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ */
+
 import com.werken.werkflow.definition.MessageCorrelator;
-import com.werken.werkflow.service.messaging.MessageSelector;
 
 import org.apache.commons.jelly.TagSupport;
 import org.apache.commons.jelly.JellyException;
 
+/** Base for custom <code>MessageCorrelator</code> tags.
+ *
+ *  <p>
+ *  Due to the pluggable nature of werkflow semantics, the
+ *  <code>AbstractMessageCorrelatorTag</code> assists in weaving custom
+ *  <code>MessageCorrelators</code>s into the fundamental syntax.
+ *  </p>
+ *
+ *  <p>
+ *  The custom tag should perform whatever is required to
+ *  construct an <code>MessageCorrelator</code> in the {@link org.apache.commons.jelly.Tag#doTag}
+ *  method, and finally call {@link #setMessageCorrelator} on this class
+ *  to install the correlator.
+ *  </p>
+ *
+ *  @see MessageCorrelator
+ *
+ *  @author <a href="mailto:bob@eng.werken.com">bob mcwhirter</a>
+ *
+ *  @version $Id$
+ */
 public abstract class AbstractMessageCorrelatorTag
     extends TagSupport
 {
+    // ----------------------------------------------------------------------
+    //     Constructors
+    // ----------------------------------------------------------------------
+
+    /** Construct.
+     */
     public AbstractMessageCorrelatorTag()
     {
+        // intentionally left blank
     }
 
+    // ----------------------------------------------------------------------
+    //     Instance methods
+    // ----------------------------------------------------------------------
+
+    /** Install a <code>MessageCorrelator</code>.
+     *
+     *  @param correlator The message correlator.
+     *
+     *  @throws JellyException If the tag is not used within the correct
+     *          context of a &lt;message&gt; tag.
+     */
     public void setMessageCorrelator(MessageCorrelator correlator)
-        throws Exception
+        throws JellyException
     {
         MessageTag message = (MessageTag) findAncestorWithClass( MessageTag.class );
 
@@ -26,6 +110,13 @@ public abstract class AbstractMessageCorrelatorTag
         message.setMessageCorrelator( correlator );
     }
 
+    /** Retrieve the message identifier.
+     *
+     *  @return The message identifier.
+     *
+     *  @throws JellyException If the tag is not used within the correct
+     *          context of a &lt;message&gt; tag.
+     */
     public String getMessageId()
         throws JellyException
     {
