c5c598d7cee3cda7d1e9486f38c602b1e663a245
bob <bob@b84b0c9b-c22a-0410-9a26-ab48ae73d5df>
2003-02-05 06:41:27 +0000

    Cleansing.

--- trunk/old.werkflow/werkflow-core/src/java/main/com/werken/werkflow/task/DefaultTask.java
+++ trunk/old.werkflow/werkflow-core/src/java/main/com/werken/werkflow/task/DefaultTask.java
@@ -2,18 +2,13 @@ package com.werken.werkflow.task;
 
 import com.werken.werkflow.action.Action;
 
-import java.util.List;
-import java.util.ArrayList;
-
 public class DefaultTask
     implements Task
 {
     private Action action;
-    private List resourceSpecs;
 
     public DefaultTask()
     {
-        this.resourceSpecs = new ArrayList();
     }
 
     public void setAction(Action action)
@@ -24,10 +19,5 @@ public class DefaultTask
     public Action getAction()
     {
         return this.action;
-    }
-
-    public ResourceSpec[] getResourceSpecs()
-    {
-        return (ResourceSpec[]) this.resourceSpecs.toArray( ResourceSpec.EMPTY_ARRAY );
     }
 }
--- trunk/old.werkflow/werkflow-core/src/java/test/com/werken/werkflow/task/MockTask.java
+++ trunk/old.werkflow/werkflow-core/src/java/test/com/werken/werkflow/task/MockTask.java
@@ -9,28 +9,16 @@ public class MockTask
     implements Task
 {
     private String id;
-    private Set resourceSpecs;
     private Action action;
 
     public MockTask(String id)
     {
         this.id = id;
-        this.resourceSpecs = new HashSet();
     }
 
     public String getId()
     {
         return this.id;
-    }
-
-    public void addResourceSpec(ResourceSpec resourceSpec)
-    {
-        this.resourceSpecs.add( resourceSpec );
-    }
-
-    public ResourceSpec[] getResourceSpecs()
-    {
-        return (ResourceSpec[]) this.resourceSpecs.toArray( ResourceSpec.EMPTY_ARRAY );
     }
 
     public void setAction(Action action)
