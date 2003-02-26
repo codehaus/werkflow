9b6ed2823e40a37b25627f537499e081d251c1f1
bob <bob@b84b0c9b-c22a-0410-9a26-ab48ae73d5df>
2003-02-26 16:34:20 +0000

    Wiring up idiom def taglib parts.

--- old.werkflow/werkflow-core/src/java/main/com/werken/werkflow/syntax/idiom/IdiomTagSupport.java
+++ old.werkflow/werkflow-core/src/java/main/com/werken/werkflow/syntax/idiom/IdiomTagSupport.java
@@ -1,5 +1,7 @@
 package com.werken.werkflow.syntax.idiom;
 
+import com.werken.werkflow.definition.petri.IdiomDefinition;
+
 import com.werken.werkflow.jelly.MiscTagSupport;
 
 import org.apache.commons.jelly.JellyTagException;
@@ -7,8 +9,29 @@ import org.apache.commons.jelly.JellyTagException;
 public abstract class IdiomTagSupport
     extends MiscTagSupport
 {
+    public static final String CURRENT_IDIOM_DEFINITION_KEY = "werkflow.idiom.definition";
+
     public IdiomTagSupport()
     {
         // intentionally left blank
+    }
+
+    public IdiomDefinition getCurrentIdiomDefinition()
+        throws JellyTagException
+    {
+        IdiomDefinition idiomDef = (IdiomDefinition) getContext().getVariable( CURRENT_IDIOM_DEFINITION_KEY );
+
+        if ( idiomDef == null )
+        {
+            throw new JellyTagException( "no current idiom definition" );
+        }
+
+        return idiomDef;
+    }
+
+    public void setCurrentIdiomDefinition(IdiomDefinition idiomDef)
+    {
+        getContext().setVariable( CURRENT_IDIOM_DEFINITION_KEY,
+                                  idiomDef );
     }
 }
