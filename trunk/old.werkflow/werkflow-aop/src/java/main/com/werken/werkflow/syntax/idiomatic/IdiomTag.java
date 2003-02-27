681e2ea49d48394470a6a3bce4e966314af2a4ac
bob <bob@b84b0c9b-c22a-0410-9a26-ab48ae73d5df>
2003-02-27 06:50:25 +0000

    * 2nd-half of idiomatic tag-lib re-org. * Replaced hand-coded tag-lib with dynamic taglib that generates tags   based upon IdiomDefinitions which themselves have been created   using an XML syntax.  Extend the idiomatic syntax by writing XML.

---
+++ trunk/old.werkflow/werkflow-aop/src/java/main/com/werken/werkflow/syntax/idiomatic/IdiomTag.java
@@ -0,0 +1,133 @@
+package com.werken.werkflow.syntax.idiomatic;
+
+import com.werken.werkflow.definition.petri.Idiom;
+import com.werken.werkflow.definition.petri.IdiomDefinition;
+import com.werken.werkflow.definition.petri.IdiomException;
+import com.werken.werkflow.definition.petri.NoSuchParameterException;
+
+import org.apache.commons.jelly.DynaTagSupport;
+import org.apache.commons.jelly.JellyContext;
+import org.apache.commons.jelly.XMLOutput;
+import org.apache.commons.jelly.JellyTagException;
+
+import java.util.Stack;
+
+public class IdiomTag
+    extends DynaTagSupport
+{
+    private static final String IDIOM_STACK_KEY = "werkflow.idiom.stack";
+
+    private IdiomDefinition idiomDef;
+    private Idiom idiom;
+
+    public IdiomTag(IdiomDefinition idiomDef)
+    {
+        this.idiomDef = idiomDef;
+    }
+
+    public IdiomDefinition getIdiomDefinition()
+    {
+        return this.idiomDef;
+    }
+
+    public void setAttribute(String id,
+                             Object value)
+        throws JellyTagException
+    {
+
+    }
+
+    public Class getAttributeType(String id)
+        throws JellyTagException
+    {
+        try
+        {
+            return getIdiomDefinition().getParameter( id ).getType();
+        }
+        catch (NoSuchParameterException e)
+        {
+            // swallow
+        }
+
+        return Object.class;
+    }
+
+    public void setContext(JellyContext context)
+        throws JellyTagException
+    {
+        super.setContext( context );
+
+        Idiom current = currentIdiom();
+
+        if ( current == null )
+        {
+            this.idiom = getIdiomDefinition().newIdiom();
+        }
+        else
+        {
+            this.idiom = current.addComponent( getIdiomDefinition() );
+        }
+
+    }
+
+    public void doTag(XMLOutput output)
+        throws JellyTagException
+    {
+        try
+        {
+            this.idiom.build();
+            
+            pushIdiom( this.idiom );
+            
+            invokeBody( output );
+            
+            popIdiom();
+        }
+        catch (IdiomException e)
+        {
+            throw new JellyTagException( e );
+        }
+    }
+
+    protected void pushIdiom(Idiom idiom)
+    {
+        Stack stack = (Stack) getContext().getVariable( IDIOM_STACK_KEY );
+
+        if ( stack == null )
+        {
+            stack = new Stack();
+            getContext().setVariable( IDIOM_STACK_KEY,
+                                      stack );
+        }
+
+        stack.push( idiom );
+    }
+
+    protected void popIdiom()
+    {
+        Stack stack = (Stack) getContext().getVariable( IDIOM_STACK_KEY );
+
+        if ( stack == null
+             ||
+             stack.isEmpty() )
+        {
+            return;
+        }
+
+        stack.pop();
+    }
+
+    protected Idiom currentIdiom()
+    {
+        Stack stack = (Stack) getContext().getVariable( IDIOM_STACK_KEY );
+
+        if ( stack == null
+             ||
+             stack.isEmpty() )
+        {
+            return null;
+        }
+
+        return (Idiom) stack.peek();
+    }
+}
