d1a8235455c498e39803c45af88b12c200e48c8b
bob <bob@b84b0c9b-c22a-0410-9a26-ab48ae73d5df>
2003-04-01 01:17:58 +0000

    Replacement for com.werken.werkflow.service.caserepo.*

---
+++ trunk/old.werkflow/werkflow-core/src/java/main/com/werken/werkflow/service/persistence/CaseTransfer.java
@@ -0,0 +1,12 @@
+package com.werken.werkflow.service.persistence;
+
+import java.util.Map;
+
+public interface CaseTransfer
+{
+    static final CaseTransfer[] EMPTY_ARRAY = new CaseTransfer[0];
+
+    String getCaseId();
+
+    Map getAttributes();
+}
---
+++ trunk/old.werkflow/werkflow-core/src/java/main/com/werken/werkflow/service/persistence/ConsumptionTransfer.java
@@ -0,0 +1,6 @@
+package com.werken.werkflow.service.persistence;
+
+public interface ConsumptionTransfer
+{
+    static final ConsumptionTransfer[] EMPTY_ARRAY = new ConsumptionTransfer[0];
+}
