e6c19573677a18150ad0be3f00f66ecefc55696d
jvanzyl <jvanzyl@b84b0c9b-c22a-0410-9a26-ab48ae73d5df>
2003-07-06 20:37:50 +0000

    o Getting rid of unused imports and variables before I tweak.

--- old.werkflow/werkflow-core/src/java/main/com/werken/werkflow/core/Executor.java
+++ old.werkflow/werkflow-core/src/java/main/com/werken/werkflow/core/Executor.java
@@ -1,29 +1,19 @@
 package com.werken.werkflow.core;
 
-import com.werken.werkflow.ProcessInfo;
+import EDU.oswego.cs.dl.util.concurrent.LinkedQueue;
+import EDU.oswego.cs.dl.util.concurrent.PooledExecutor;
 import com.werken.werkflow.Attributes;
-import com.werken.werkflow.NoSuchCaseException;
-import com.werken.werkflow.NoSuchProcessException;
-import com.werken.werkflow.definition.ProcessDefinition;
-import com.werken.werkflow.definition.Waiter;
-import com.werken.werkflow.definition.MessageWaiter;
-import com.werken.werkflow.definition.petri.Net;
 import com.werken.werkflow.definition.petri.Arc;
 import com.werken.werkflow.definition.petri.Place;
 import com.werken.werkflow.definition.petri.Transition;
-import com.werken.werkflow.definition.petri.ActivationRule;
-import com.werken.werkflow.definition.petri.NoSuchTransitionException;
+import com.werken.werkflow.expr.AttributesExpressionContext;
 import com.werken.werkflow.expr.Expression;
 import com.werken.werkflow.expr.ExpressionContext;
-import com.werken.werkflow.expr.AttributesExpressionContext;
 import com.werken.werkflow.service.messaging.Message;
 import com.werken.werkflow.service.persistence.PersistenceException;
 
-import EDU.oswego.cs.dl.util.concurrent.PooledExecutor;
-import EDU.oswego.cs.dl.util.concurrent.LinkedQueue;
-
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
 class Executor
     implements ActivityCompleter
@@ -49,16 +39,16 @@ class Executor
 
     }
 
-    void enqueueActivities(CoreActivity[] activities)
+    void enqueueActivities( CoreActivity[] activities )
         throws InterruptedException
     {
-        for ( int i = 0 ; i < activities.length ; ++i )
+        for ( int i = 0; i < activities.length; ++i )
         {
             enqueueActivity( activities[i] );
         }
     }
 
-    void enqueueActivity(CoreActivity activity)
+    void enqueueActivity( CoreActivity activity )
         throws InterruptedException
     {
         System.err.println( "enqueue: " + activity );
@@ -66,18 +56,14 @@ class Executor
                                            activity ) );
     }
 
-    void enqueue(ExecutionEnqueuement enqueuement)
+    void enqueue( ExecutionEnqueuement enqueuement )
         throws InterruptedException
     {
         this.queue.put( enqueuement );
     }
 
-    void execute(CoreActivity activity)
+    void execute( CoreActivity activity )
     {
-        CoreWorkItem    workItem    = activity.getWorkItem();
-        CoreProcessCase processCase = workItem.getCase();
-        ProcessInfo     processInfo = processCase.getProcessInfo();
-
         Map otherAttrs = new HashMap();
 
         Message message = activity.getMessage();
@@ -97,62 +83,62 @@ class Executor
         activity.perform( invocation );
     }
 
-    public void complete(CoreActivity activity,
-                         Attributes caseAttrs)
+    public void complete( CoreActivity activity,
+                          Attributes caseAttrs )
     {
         CoreProcessCase processCase = activity.getWorkItem().getCase();
-        
+
         synchronized ( processCase )
         {
             ChangeSetSource changeSetSource = processCase.getChangeSetSource();
-            
+
             CoreChangeSet changeSet = changeSetSource.newChangeSet();
 
             Transition transition = activity.getWorkItem().getTransition();
-            
+
             transferAttributes( changeSet,
                                 processCase,
                                 caseAttrs );
-            
+
             produceTokens( changeSet,
                            processCase,
                            transition );
-            
+
             try
             {
                 changeSet.commit();
             }
-            catch (PersistenceException e)
+            catch ( PersistenceException e )
             {
                 e.printStackTrace();
             }
         }
     }
 
-    void transferAttributes(CoreChangeSet changeSet,
-                            CoreProcessCase processCase,
-                            Attributes caseAttrs)
+    void transferAttributes( CoreChangeSet changeSet,
+                             CoreProcessCase processCase,
+                             Attributes caseAttrs )
     {
         String[] attrNames = caseAttrs.getAttributeNames();
 
-        for ( int i = 0 ; i < attrNames.length ; ++i )
+        for ( int i = 0; i < attrNames.length; ++i )
         {
             processCase.setAttribute( attrNames[i],
                                       caseAttrs.getAttribute( attrNames[i] ) );
         }
-        
+
         changeSet.addModifiedCase( processCase );
     }
 
-    void produceTokens(CoreChangeSet changeSet,
-                       CoreProcessCase processCase,
-                       Transition transition)
+    void produceTokens( CoreChangeSet changeSet,
+                        CoreProcessCase processCase,
+                        Transition transition )
     {
-        Arc[] arcs  = transition.getArcsToPlaces();
+        Arc[] arcs = transition.getArcsToPlaces();
 
         ExpressionContext context = new AttributesExpressionContext( processCase );
 
-        for ( int i = 0 ; i < arcs.length ; ++i )
+        for ( int i = 0; i < arcs.length; ++i )
         {
             Expression expr = arcs[i].getExpression();
 
@@ -160,12 +146,12 @@ class Executor
             {
                 try
                 {
-                    if ( ! expr.evaluateAsBoolean( context ) )
+                    if ( !expr.evaluateAsBoolean( context ) )
                     {
                         continue;
                     }
                 }
-                catch (Exception e)
+                catch ( Exception e )
                 {
                     // INTERNAL ERROR
                     e.printStackTrace();
@@ -176,18 +162,18 @@ class Executor
 
             processCase.addToken( place.getId() );
         }
-        
+
         changeSet.addModifiedCase( processCase );
     }
 
-    public void completeWithError(CoreActivity activity,
-                                  Throwable error)
+    public void completeWithError( CoreActivity activity,
+                                   Throwable error )
     {
         CoreProcessCase processCase = activity.getWorkItem().getCase();
 
         synchronized ( processCase )
         {
             error.printStackTrace();
-        }      
+        }
     }
 }
