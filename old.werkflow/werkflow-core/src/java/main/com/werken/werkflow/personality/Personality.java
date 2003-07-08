493dae7271f15cd8311bcc3a0c77efd8b14a5432
mhw <mhw@b84b0c9b-c22a-0410-9a26-ab48ae73d5df>
2003-07-08 13:59:05 +0000

    Add load(URL, Map)

--- old.werkflow/werkflow-core/src/java/main/com/werken/werkflow/personality/Personality.java
+++ old.werkflow/werkflow-core/src/java/main/com/werken/werkflow/personality/Personality.java
@@ -1,12 +1,16 @@
 package com.werken.werkflow.personality;
 
-import com.werken.werkflow.definition.ProcessDefinition;
-
 import java.io.IOException;
 import java.net.URL;
+import java.util.Map;
+
+import com.werken.werkflow.definition.ProcessDefinition;
 
 public interface Personality
 {
     ProcessDefinition[] load(URL url)
+        throws IOException, Exception;
+
+    ProcessDefinition[] load(URL url, Map beans)
         throws IOException, Exception;
 }
