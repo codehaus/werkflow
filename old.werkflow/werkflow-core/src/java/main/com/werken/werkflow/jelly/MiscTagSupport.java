ceeabd059e4902a37f976ec4c5cfff70d80c5872
mhw <mhw@b84b0c9b-c22a-0410-9a26-ab48ae73d5df>
2003-07-31 14:32:47 +0000

    Update with codehaus copyright and license.

--- old.werkflow/werkflow-core/src/java/main/com/werken/werkflow/jelly/MiscTagSupport.java
+++ old.werkflow/werkflow-core/src/java/main/com/werken/werkflow/jelly/MiscTagSupport.java
@@ -3,7 +3,7 @@ package com.werken.werkflow.jelly;
 /*
  $Id$
 
- Copyright 2003 (C) The Werken Company. All Rights Reserved.
+ Copyright 2003 (C) The Codehaus. All Rights Reserved.
 
  Redistribution and use of this software and associated documentation
  ("Software"), with or without modification, are permitted provided
@@ -20,22 +20,22 @@ package com.werken.werkflow.jelly;
 
  3. The name "werkflow" must not be used to endorse or promote
     products derived from this Software without prior written
-    permission of The Werken Company.  For written permission,
-    please contact bob@werken.com.
+    permission of The Codehaus.  For written permission,
+    please contact info@codehaus.org.
 
  4. Products derived from this Software may not be called "werkflow"
     nor may "werkflow" appear in their names without prior written
-    permission of The Werken Company. "werkflow" is a registered
-    trademark of The Werken Company.
+    permission of The Codehaus. "werkflow" is a registered
+    trademark of The Codehaus.
 
- 5. Due credit should be given to The Werken Company.
-    (http://werkflow.werken.com/).
+ 5. Due credit should be given to The Codehaus -
+    http://werkflow.codehaus.org/
 
- THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS
+ THIS SOFTWARE IS PROVIDED BY THE CODEHAUS AND CONTRIBUTORS
  ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
  NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
- THE WERKEN COMPANY OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ THE CODEHAUS OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
