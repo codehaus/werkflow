c3efa505c6f4682bcd40942756e6be98867e4833
bselders <bselders@b84b0c9b-c22a-0410-9a26-ab48ae73d5df>
2004-10-29 23:26:49 +0000

    Just get a working process in here as example o Added a workflow that uses the interactive choice idiom, based on an example that bob provided me. o Added some methods to interfaces. o Added simple memory persistence manager implementation needed for this example.

--- trunk/werkflow/src/main/java/org/codehaus/werkflow/spi/Instance.java
+++ trunk/werkflow/src/main/java/org/codehaus/werkflow/spi/Instance.java
@@ -5,8 +5,12 @@ import org.codehaus.werkflow.Context;
 public interface Instance
     extends Context
 {
+    public String getId();
+
     void put(String name,
              Object value);
 
     Path[] getActiveChildren(Path path);
+
+    public SatisfactionSpec[] getBlockedSatisfactions();
 }
