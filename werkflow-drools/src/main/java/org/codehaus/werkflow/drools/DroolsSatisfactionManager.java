66c355627009fcace49f97446b2ab5517260e1f5
mhw <mhw@b84b0c9b-c22a-0410-9a26-ab48ae73d5df>
2004-12-02 17:47:02 +0000

    Rework for newer SatisfactionManager interface. Most of the functionality has now moved to SimpleSatisfactionManager.

--- werkflow-drools/src/main/java/org/codehaus/werkflow/drools/DroolsSatisfactionManager.java
+++ werkflow-drools/src/main/java/org/codehaus/werkflow/drools/DroolsSatisfactionManager.java
@@ -1,134 +1,102 @@
 package org.codehaus.werkflow.drools;
 
 import org.codehaus.werkflow.Context;
-import org.codehaus.werkflow.spi.SatisfactionManager;
+import org.codehaus.werkflow.Engine;
+import org.codehaus.werkflow.NoSuchInstanceException;
+import org.codehaus.werkflow.Transaction;
+import org.codehaus.werkflow.helpers.SimpleSatisfactionManager;
+import org.codehaus.werkflow.spi.DefaultSatisfactionValues;
+import org.codehaus.werkflow.spi.RobustTransaction;
 import org.codehaus.werkflow.spi.SatisfactionCallback;
+import org.codehaus.werkflow.spi.SatisfactionManager;
 
-import org.drools.FactHandle;
 import org.drools.WorkingMemory;
 import org.drools.RuleBase;
 import org.drools.FactException;
 
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Set;
-import java.util.HashSet;
-
 public class DroolsSatisfactionManager
+    extends SimpleSatisfactionManager
     implements SatisfactionManager
 {
     public static final String APPDATA_KEY = "drools.satisfaction.manager";
 
-    private Map satisfactions;
-    private Map callbacks;
+    private static final ThreadLocal threadTransaction = new ThreadLocal();
+
+    private Engine engine;
 
     private WorkingMemory workingMemory;
 
-    public DroolsSatisfactionManager(RuleBase ruleBase)
+    public DroolsSatisfactionManager(RuleBase ruleBase,
+                                     Engine engine)
     {
-        this.satisfactions = new HashMap();
-        this.callbacks     = new HashMap();
+        this.engine = engine;
         this.workingMemory = ruleBase.newWorkingMemory();
         this.workingMemory.getApplicationDataMap().put( APPDATA_KEY,
                                                         this );
     }
 
-    protected  WorkingMemory getWorkingMemory()
+    protected WorkingMemory getWorkingMemory()
     {
         return this.workingMemory;
     }
 
-    public boolean isSatisfied(String satisfactionId,
-                               Context context)
-    {
-        return isSatisfied( satisfactionId,
-                            context.getId() );
-    }
-
-    protected synchronized boolean isSatisfied(String satisfactionId,
-                                               String contextId)
-    {
-        Set contextIds = (Set) this.satisfactions.get( satisfactionId );
-
-        if ( contextIds != null )
-        {
-            return contextIds.contains( contextId );
-        }
-
-        return false;
-    }
-
-    public synchronized void notifySatisfied(String satisfactionId,
-                                             String contextId)
-    {
-        Set contextIds = (Set) this.satisfactions.get( satisfactionId );
-        
-        if ( contextIds == null )
-        {
-            contextIds = new HashSet();
-            this.satisfactions.put( satisfactionId,
-                                    contextIds );
-        }
-
-        contextIds.add( contextId );
-
-        Map callbacks = (Map) this.callbacks.get( satisfactionId );
-
-        if ( callbacks != null )
-        {
-            SatisfactionCallback callback = (SatisfactionCallback) callbacks.get( contextId );
-
-            if ( callback != null )
-            {
-                callback.notifySatisfied();
-            }
-        }
-    }
-    
-    public synchronized boolean isSatisfied(String satisfactionId,
+    public synchronized boolean isSatisfied(RobustTransaction transaction,
+                                            String satisfactionId,
                                             Context context,
                                             SatisfactionCallback callback)
     {
-        if ( isSatisfied( satisfactionId,
-                          context ) )
-        {
-            return true;
-        }
+        threadTransaction.set( transaction );
 
+        boolean isSatisfied = super.isSatisfied( transaction,
+                                                 satisfactionId,
+                                                 context,
+                                                 callback );
+
+        threadTransaction.set( null );
+
+        return isSatisfied;
+    }
+
+    protected void provokeSatisfactions(String satisfactionId,
+                                        Context context)
+    {
         try
         {
             getWorkingMemory().assertObject( setUpRequest( context ) );
             getWorkingMemory().fireAllRules();
-            
-            boolean isSatisfied = isSatisfied( satisfactionId,
-                                               context.getId() );
-
-            if ( ! isSatisfied )
-            {
-                Map callbacks = (Map) this.callbacks.get( satisfactionId );
-                
-                if ( callbacks == null )
-                {
-                    callbacks = new HashMap();
-                    this.callbacks.put( satisfactionId,
-                                        callbacks );
-                }
-                
-                callbacks.put( context.getId(),
-                               callback );
-            }
-
-            return isSatisfied;
         }
         catch (FactException e)
         {
             // TODO: set error flag
-            return false;
         }
     }
 
     protected Request setUpRequest(Context context)
     {
         return new ContextRequest( context );
+    }
+
+    public void notifySatisfied(String satisfactionId,
+                                String contextId)
+        throws NoSuchInstanceException, InterruptedException, Exception
+    {
+        Transaction tx = (Transaction) threadTransaction.get();
+
+        boolean localTransaction = false;
+
+        if ( tx == null )
+        {
+            localTransaction = true;
+
+            tx = engine.beginTransaction( contextId );
+        }
+
+        tx.satisfy( satisfactionId,
+                    new DefaultSatisfactionValues() );
+
+        if ( localTransaction )
+        {
+            tx.commit();
+        }
     }
 }
