8f302968f270271709d3854adf3ab5ba38e96263
jvanzyl <jvanzyl@b84b0c9b-c22a-0410-9a26-ab48ae73d5df>
2005-06-01 19:51:41 +0000

    o adding error handling spi classes

---
+++ trunk/werkflow/src/main/java/org/codehaus/werkflow/spi/DefaultErrorHandler.java
@@ -0,0 +1,13 @@
+package org.codehaus.werkflow.spi;
+
+/**
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ * @version $Id$
+ */
+public class DefaultErrorHandler
+    implements ErrorHandler
+{
+    public void handle( Throwable throwable )
+    {        
+    }
+}
