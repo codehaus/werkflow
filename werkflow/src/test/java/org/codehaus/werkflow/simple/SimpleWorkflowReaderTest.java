1cd5375c7d0ab28bb2fc28198a818b5717d17dd2
clim <clim@b84b0c9b-c22a-0410-9a26-ab48ae73d5df>
2005-08-10 18:18:50 +0000

    Added more test cases.

--- werkflow/src/test/java/org/codehaus/werkflow/simple/SimpleWorkflowReaderTest.java
+++ werkflow/src/test/java/org/codehaus/werkflow/simple/SimpleWorkflowReaderTest.java
@@ -1,32 +1,90 @@
 package org.codehaus.werkflow.simple;
 
-import org.codehaus.werkflow.spi.Instance;
+import java.net.URL;
+import java.io.File;
+
 import org.codehaus.werkflow.Workflow;
+import org.codehaus.werkflow.helpers.MockActionManager;
 
-import java.util.Properties;
-
+/*
+ * $Id$
+ */
 public class SimpleWorkflowReaderTest
     extends SimpleTestBase
-    implements ActionManager
 {
-    public void testValidWorkflow()
+
+    private ActionManager actionManager;
+    private ExpressionFactory expressionFactory;
+
+    protected void setUp() throws Exception {
+
+        actionManager = new MockActionManager();
+        expressionFactory = new MockExpressionFactory();
+
+    }
+
+    protected void tearDown() throws Exception {
+
+        actionManager = null;
+        expressionFactory = null;
+
+    }
+
+    public void testReadFromFile()
         throws Exception
     {
-        MockExpressionFactory exprFactory = new MockExpressionFactory();
 
-        Workflow workflow = SimpleWorkflowReader.read( this,
-                                                       exprFactory,
-                                                       getClass().getResource( "valid.xml" ) );
+        File file = new File( "target/test-classes/org/codehaus/werkflow/simple/valid.xml" );
+
+        Workflow workflow = SimpleWorkflowReader.read(
+            actionManager,
+            expressionFactory,
+            file );
 
         assertNotNull( workflow );
-        
+
     }
 
-    public void perform(String actionId,
-                        Instance instance,
-                        Properties props)
+    public void testReadFromInputStream()
         throws Exception
     {
-        
+
+        Workflow workflow = SimpleWorkflowReader.read(
+            actionManager,
+            expressionFactory,
+            getClass().getResourceAsStream( "valid.xml" ) );
+
+        assertNotNull( workflow );
+
     }
+
+    public void testReadFromURL()
+        throws Exception
+    {
+
+        File file = new File( "target/test-classes/org/codehaus/werkflow/simple/valid.xml" );
+        URL url = file.toURL();
+
+        Workflow workflow = SimpleWorkflowReader.read(
+            actionManager,
+            expressionFactory,
+            url);
+
+        assertNotNull( workflow );
+
+    }
+
+    public void testReadFromInputSource()
+        throws Exception
+    {
+
+        Workflow workflow = SimpleWorkflowReader.read(
+            actionManager,
+            expressionFactory,
+            getClass().getResource( "valid.xml" ) );
+
+        assertNotNull( workflow );
+
+    }
+
 }
